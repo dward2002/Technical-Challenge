9d3dd47c37a4ff9ef483b571fcdaa8ef690d52f0
Dward <d.ward4@wlv.ac.uk>
2024-06-01 13:40:00 +0100

    Add working functionality to the functions in CheckerService

--- src/Service/CheckerService.php
+++ src/Service/CheckerService.php
@@ -8,16 +8,33 @@ class CheckerService implements CheckerInterface
 {
     public function isPalindrome($word): bool
     {
-        return false;
+        $cleanedWord = strtolower(preg_replace('/[^a-z0-9]/', '', $word));
+        return $cleanedWord === strrev($cleanedWord);
     }
 
     public function isAnagram(string $word, string $comparison) : bool
     {
-        return false;
+        $cleanedWord = strtolower(preg_replace('/[^a-z0-9]/', '', $word));
+        $cleanedComparison = strtolower(preg_replace('/[^a-z0-9]/', '', $comparison));
+        $wordArray = str_split($cleanedWord);
+        $comparisonArray = str_split($cleanedComparison);
+        sort($wordArray);
+        sort($comparisonArray);
+
+        return $wordArray === $comparisonArray;
     }
 
     public function isPangram(string $phrase) : bool
     {
-        return false;
+        $cleanedPhrase = strtolower(preg_replace('/[^a-z0-9]/', '', $phrase));
+        $alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        
+        foreach($alphabet as $letter) {
+            if (strpos($cleanedPhrase, $letter) === false) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
