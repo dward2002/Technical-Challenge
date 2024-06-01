0a74e0508ed22d93a8d3481d863e491e743e8e3f
Dward <d.ward4@wlv.ac.uk>
2024-06-01 13:43:55 +0100

    Refactor isPangram method to use range for generating alphabet array

--- src/Service/CheckerService.php
+++ src/Service/CheckerService.php
@@ -27,7 +27,7 @@ class CheckerService implements CheckerInterface
     public function isPangram(string $phrase) : bool
     {
         $cleanedPhrase = strtolower(preg_replace('/[^a-z0-9]/', '', $phrase));
-        $alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        $alphabet = range('a', 'z');
         
         foreach($alphabet as $letter) {
             if (strpos($cleanedPhrase, $letter) === false) {
