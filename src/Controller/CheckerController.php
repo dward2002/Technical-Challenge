0b939cb2e39314270cddfe3b3d693a82d354cf77
Dward <d.ward4@wlv.ac.uk>
2024-06-03 10:52:51 +0100

    Add API endpoints and testing for each method of checkerService

--- src/Controller/CheckerController.php
+++ src/Controller/CheckerController.php
@@ -20,10 +20,24 @@ class CheckerController extends AbstractController
         return $this->render('homepage/homepage.html.twig');
     }
 
+    #[Route('/palindrome/{word}', name: 'app_palindrome_check')]
+    public function isPalindrome(string $word): JsonResponse
+    {
+        $result = $this->checkerService->isPalindrome($word);
+        return new JsonResponse(['word' => $word, 'isPalindrome' => $result]);
+    }
+
     #[Route('/anagram/{word}/{comparison}', name: 'app_anagram_check')]
     public function isAnagram(string $word, string $comparison): JsonResponse
     {
         $result = $this->checkerService->isAnagram($word, $comparison);
         return new JsonResponse(['word' => $word, 'comparison' => $comparison, 'isAnagram' => $result]);
+    }
+
+    #[Route('/pangram/{phrase}', name: 'app_pangram_check')]
+    public function isPangram(string $phrase): JsonResponse
+    {
+        $result = $this->checkerService->isPangram($phrase);
+        return new JsonResponse(['phrase' => $phrase, 'isPangram' => $result]);
     }
 }
--- tests/controller/CheckerControllerTest.php
+++ tests/controller/CheckerControllerTest.php
@@ -13,7 +13,22 @@ class CheckerControllerTest extends WebTestCase
         $this->assertSelectorTextContains('h5', 'Anagram');
     }
 
-    public function testCheckAnagram()
+    public function testIsPalindrome()
+    {
+        $client = static::createClient();
+
+        $word = 'anna';
+
+        $client->request(
+            'GET',
+            sprintf('/palindrome/%s', $word),
+        );
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonStringEqualsJsonString('{"word":"' . $word . '","isPalindrome":true}', $client->getResponse()->getContent());
+    }
+
+    public function testIsAnagram()
     {
         $client = static::createClient();
 
@@ -27,5 +42,20 @@ class CheckerControllerTest extends WebTestCase
 
         $this->assertResponseIsSuccessful();
         $this->assertJsonStringEqualsJsonString('{"word":"' . $word . '","comparison":"' . $comparison . '","isAnagram":true}', $client->getResponse()->getContent());
+    }
+
+    public function testIsPangram()
+    {
+        $client = static::createClient();
+
+        $word = 'The quick brown fox jumps over the lazy dog';
+
+        $client->request(
+            'GET',
+            sprintf('/pangram/%s', $word),
+        );
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonStringEqualsJsonString('{"phrase":"' . $word . '","isPangram":true}', $client->getResponse()->getContent());
     }
 }
